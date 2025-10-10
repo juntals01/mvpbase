dbf34e7955d6a7b42b42b71233cb3b9f928c9bdd
jun <norbertoqjr@gmail.com>
2025-10-10 14:26:37 +0800

    adds docker service: firebase, postgress,redis,minio

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Page() {
-  return <div className='bg-red-500'>OK</div>;
+  return (
+    <div className='bg-red-500'>OK {`${process.env.NEXT_PUBLIC_API_URL}`}</div>
+  );
 }
